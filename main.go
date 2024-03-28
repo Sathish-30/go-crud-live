744965e75effa98c57f143e8da3480d7d226efe6
sathish-30 <sathish.amsaravanan@gmail.com>
2024-03-28 22:36:09 +0530

    Initialized a golang proj with docker

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	BookId     int    `json:"bookId"`
+	BookName   string `json:"bookName"`
+	AuthorName string `json:"authorName"`
+	gorm.Model
+}
+
+var db *gorm.DB
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Failed to load env")
+	}
+}
+
+func main() {
+	const PORT string = ":8080"
+	server := http.NewServeMux()
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err := db.AutoMigrate(&Book{}); err != nil {
+		log.Fatal("Failed to migrate database")
+	}
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	log.Println("Database connected")
+	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]string{
+			"msg": "In Home route",
+		})
+	})
+	server.HandleFunc("GET /getBooks", getAllBooksHandler)
+	http.ListenAndServe(PORT, server)
+}
+
+func getAllBooksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"msg": "health check",
+	})
+	w.WriteHeader(http.StatusAccepted)
+}
